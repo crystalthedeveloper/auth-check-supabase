fc04daadc3805e7f7b32827870f0cd9a2c68a685
crystallewisdeveloper <107262091+crystallewisdeveloper@users.noreply.github.com>
2024-12-16 14:05:40 -0500

    login v13

--- login.js
+++ login.js
@@ -1,13 +1,11 @@
 
 // Login
-document.addEventListener("DOMContentLoaded", () => {
-    // Supabase configuration
+document.addEventListener("DOMContentLoaded", async () => {
+    // Ensure a single Supabase client instance
     const SUPABASE_URL = "https://pkaeqqqxhkgosfppzmmt.supabase.co";
     const SUPABASE_KEY =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInBrYWVxcXF4aGtnb3NmcHB6bW10Iiwicm9zZSI6ImFub24iLCJpYXQiOjE3MzQyNzEyMjgsImV4cCI6MjA0OTg0NzIyOH0.dpxd-Y6Zvfu_1tcfELPNV7acq6X9tWMd8paNK28ncsc";
-
-    // Initialize Supabase client
-    const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrYWVxcXF4aGtnb3NmcHB6bW10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQyNzEyMjgsImV4cCI6MjA0OTg0NzIyOH0.dpxd-Y6Zvfu_1tcfELPNV7acq6X9tWMd8paNK28ncsc";
+    const supabase = window.supabase || supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
     const loginForm = document.querySelector("#login-form");
 
